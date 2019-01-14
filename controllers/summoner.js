6cc6f8bf8fd44ea2ac7e49adb60ec9d7a67fae51
Ryan Doucette <31770773+RyanJLDoucette@users.noreply.github.com>
2019-01-14 14:37:21 -0400

    Added some error handling for when user searches for a summoner that does not exist or searches for a summoner that does not have ranked stats. It now renders a page that tells the users what happens. In the future instead of rendering this page, it would be a better idea to return to search/home page with a message above the search bar in red letters telling the user they searched for an invalid summoner.
    I also added go back functionalities to the display summoner page, as well as the invalid summoner page.

--- controllers/summoner.js
+++ controllers/summoner.js
@@ -15,9 +15,13 @@ var getSummoner = require('../helpers/createSummoner.js');
 // SUMMONER PAGE - displays information about the summoner searched for in the landing page
 router.get("/summoner", function(req, res){
    var summonerName = req.query.name; //req.query is what was inputted from the form on the landing page. The input id was name.
-   //var summoner = getSummoner.createSummoner(summonerName);//This is a call to a helper function that takes the summoner name and uses the riot games API to populate a "Summoner Object" which contains the relevant data of the summoner to be displayed to the user.
    getSummoner.createSummoner(summonerName, function(receivedSummoner) {
-      res.render("summoner", {summoner: receivedSummoner});
+      if(receivedSummoner) {
+         res.render("summoner", {summoner: receivedSummoner});
+      } else {
+         res.render("invalid_summoner");
+      }
+      
    });
 });
 
--- helpers/createSummoner.js
+++ helpers/createSummoner.js
@@ -16,47 +16,56 @@ module.exports = {
             if (error) {
                 console.log("Error with the API  summoner by name request");
                 console.log(error);
-            } else {
-                if (response.statusCode = 200) {// Response 200 indicates Successful api call
+            } else {//The call to the API was valid format, but we still need to check the response code for success
+                if (response.statusCode == 200) {// Response 200 indicates Successful api calls
+                    // DEBUG console.log("https://na1.api.riotgames.com/lol/summoner/v4/summoners/by-name/" + summonerName + apiKey);
                     var parsedData = JSON.parse(body);//puts the data in a usable format
                     var name = parsedData.name;
                     var icon = parsedData.profileIconId;
                     var level = parsedData.summonerLevel;
                     var summonerId = parsedData.id;//This is an encrypted Id. We can use to gain more information on this summoner which we will do below.
-                }
-                request("https://na1.api.riotgames.com/lol/league/v4/positions/by-summoner/" + summonerId + apiKey, function (error, response, body) {
-                    if (error) {
-                        console.log("Error with the API position by summoner request");
-                        console.log(error);
-                    } else {
-                        if (response.statusCode = 200) {// Response 200 indicates Successful api call
-                            var parsedData = JSON.parse(body);//puts it into a format we can use
-                            var rank = {
-                                //NOTE: parsedData contains two arrays of information. Information at index 0 contains the statistics for ranked flex. We want solo/duo queue information which resides at
-                                //at index 1 of the array. 
-                                tier: parsedData[1].tier, //Example: Silver
-                                division: parsedData[1].rank, //Example: 3
-                                lp: parsedData[1].leaguePoints//Example: 27
-                            };
-                            var wins = parsedData[1].wins;
-                            var losses = parsedData[1].losses;
+                    request("https://na1.api.riotgames.com/lol/league/v4/positions/by-summoner/" + summonerId + apiKey, function (error, response, body) {
+                        if (error) {
+                            console.log("Error with the API position by summoner request");
+                            console.log(error);
+                        } else {
+                            if (response.statusCode == 200 && JSON.parse(body) != "") {// Response 200 indicates Successful api call. 2nd param is needed because if summoner has no ranked stats it will be status code 200 but empty object returned. 
+                                var parsedData = JSON.parse(body);//puts it into a format we can use
+                                console.log("https://na1.api.riotgames.com/lol/league/v4/positions/by-summoner/" + summonerId + apiKey);
+                                var rank = {
+                                    //NOTE: parsedData contains two arrays of information. Information at index 0 contains the statistics for ranked flex. We want solo/duo queue information which resides at
+                                    //at index 1 of the array. 
+                                    tier: parsedData[1].tier, //Example: Silver
+                                    division: parsedData[1].rank, //Example: 3
+                                    lp: parsedData[1].leaguePoints//Example: 27
+                                };
+                                var wins = parsedData[1].wins;
+                                var losses = parsedData[1].losses;
+                                //For simplicity - we store all information gathered into a summoner object and return it.
+                                var summonerObject = {
+                                    name: name,
+                                    icon: icon,
+                                    level: level,
+                                    summonerId: summonerId,
+                                    rank: rank,
+                                    wins: wins,
+                                    losses: losses,
+                                    winRatio: wins / (wins + losses)
+
+                                };
+                                //console.log(summonerObject);
+                                callback(summonerObject);
+                            }
+                            else {// Summoner has no ranked stats
+                                callback(false);
+                            }
                         }
-                        //For simplicity - we store all information gathered into a summoner object and return it.
-                        var summonerObject = {
-                            name: name,
-                            icon: icon,
-                            level: level,
-                            summonerId: summonerId,
-                            rank: rank,
-                            wins: wins,
-                            losses: losses,
-                            winRatio: wins/(wins + losses)
-                        };
-                        //console.log(summonerObject);
-                        callback(summonerObject);
-                    }
-                });
+                    });//end of second call to the api
+                }
+                else {//The response code was not 200
+                    callback(1);
+                }
             }
-        });
-    }
-}
+        });//end inital call
+    }//end create summoner method
+}//end module.exports
